2e847ff986fd5391c6f129912b2ac1b7528c8466
Park-Geonhee <84301822+Park-Geonhee@users.noreply.github.com>
2021-06-04 17:09:33 +0900

    Update Delete.cs

--- Delete.cs
+++ Delete.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,47 @@ namespace DB_Management
         {
             InitializeComponent();
         }
+        public MySqlDb mySqlDb
+        {
+            get; set;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (mySqlDb?.IsOpen == true)
+            {
+                try
+                {
+                    String sql = String.Format("select * from member WHERE MEMBER_ID ={0}", this.Del_Id.Text);
+                    MySqlCommand cmd = new MySqlCommand(sql, mySqlDb.Conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+
+                    if (rdr.HasRows)
+                    {
+                        rdr.Dispose();
+                        String query = String.Format("delete from member where MEMBER_ID =('{0}')", this.Del_Id.Text);
+                        MySqlCommand cmd_insert = new MySqlCommand(query, mySqlDb.Conn);
+                        cmd_insert.ExecuteNonQuery();
+                        MessageBox.Show("성공적으로 제거되었습니다");
+                        this.Dispose();
+                    }
+                    else
+                    {
+                        MessageBox.Show("조회된 아이디가 없습니다.");
+                        rdr.Dispose();
+                    }
+                    
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message + "\r\n\r\n" + ex.StackTrace, "에러발생");
+                }
+            }
+            else
+            {
+                MessageBox.Show("서버와 연결이 되어있지 않습니다.");
+            }
+        }
+        
     }
 }
